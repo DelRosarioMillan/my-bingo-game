4e09cedf27ca5066cfc7a6c7bfbe1a0170257c70
Johnangel Del Rosario <MardukDelRosario@outlook.com>
2024-10-09 03:37:39 -0400

    add speedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
         <footer className="py-4 bg-gray-800 text-white text-center text-sm">
           <p>&copy; {new Date().getFullYear()} TSU. Del Rosario Johnangel. Todos los derechos reservados.</p>
         </footer>
