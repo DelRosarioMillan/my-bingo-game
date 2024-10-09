946d8eb9fd50eb5e4f85beddb66fa072f7be37de
Johnangel Del Rosario <MardukDelRosario@outlook.com>
2024-10-09 03:27:28 -0400

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Juego de Bingo',
   description: 'Un juego de Bingo interactivo',
+  icons: {
+    icon: '/favicon.ico',
+  },
+
 }
 
 export default function RootLayout({
@@ -16,7 +21,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+        <footer className="py-4 bg-gray-800 text-white text-center text-sm">
+          <p>&copy; {new Date().getFullYear()} TSU. Del Rosario Johnangel. Todos los derechos reservados.</p>
+        </footer>
+      </body>
     </html>
   )
 }
